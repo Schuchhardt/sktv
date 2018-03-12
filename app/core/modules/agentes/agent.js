62f373bfa80d6dc8b1c99b158d8384c7c2b89bb1
Sebastián Schuchhardt <seba.sch999@gmail.com>
2018-03-12 01:00:58 -0300

    agent detail

--- app/core/modules/agentes/agent.js
+++ app/core/modules/agentes/agent.js
@@ -1,6 +1,6 @@
 export default class AgentCtrl {
   /* @ngInject */
-  constructor($scope, $state, $rootScope, $stateParams, apiService) {
+  constructor($scope, $state, $rootScope, $stateParams, apiService, $uibModal) {
     $rootScope.currentState = $state.current.name;
     $scope.agentType = $stateParams.agentType;
 
@@ -13,6 +13,26 @@ export default class AgentCtrl {
     const loadAgent = () => {
       apiService.loadAgent($stateParams.agentType, $stateParams.agentId).then( (response) => {
         $scope.currentAgent = response.agent;
+        const allPhotos = response.agent.photos;
+        $scope.currentAgentPhoto = allPhotos.pop();
+        $scope.currentAgentPhotos = allPhotos.reverse();
+      });
+    };
+
+    $scope.showSlider = (photo) => {
+      $uibModal.open({
+        animation: false,
+        templateUrl: 'sliderPhotos',
+        controller: 'SliderPhotosCtrl',
+        size: "lg",
+        resolve: {
+          currentImage: () => {
+            return photo;
+          },
+          currentEvent: () =>{
+            return {all_photos: $scope.currentAgentPhotos};
+          }
+        }
       });
     };
 
