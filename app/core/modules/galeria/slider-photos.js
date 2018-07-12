bcbf6a25e9ded717e61c300b716cd03048c5aee7
Sebastián Schuchhardt <seba.sch999@gmail.com>
2018-07-12 12:48:00 -0400

    cambios diseño

--- app/core/modules/galeria/slider-photos.js
+++ app/core/modules/galeria/slider-photos.js
@@ -1,11 +1,15 @@
 export default class SliderPhotosCtrl {
   /* @ngInject */
-  constructor($scope, $uibModalInstance, $sce, $timeout, currentImage, currentEvent) {
+  constructor($scope, $uibModalInstance, $sce, $timeout, $window, currentImage, currentEvent) {
     $scope.currentSlide = _.indexOf(currentEvent.all_photos, currentImage, currentEvent.offseted ? 7 : 0);
     $scope.currentEvent = currentEvent;
 
     $scope.close = function () {
       $uibModalInstance.dismiss();
+    };
+
+    $scope.download = function (photo) {
+    	$window.open(photo.photo_url);
     };
 
   }
